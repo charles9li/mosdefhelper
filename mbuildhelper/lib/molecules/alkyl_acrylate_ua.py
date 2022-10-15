ba53b5cb4cc0297e2f1cdd98c51f29aec2470b23
charles9li <charles9000li@gmail.com>
2022-10-14 21:23:53 -0700

    added recipe for poly alkyl (meth)acrylates

--- mbuildhelper/lib/molecules/alkyl_acrylate_ua.py
+++ mbuildhelper/lib/molecules/alkyl_acrylate_ua.py
@@ -1,17 +1,17 @@
-__all__ = ['AlkylAcrylate', 'ButylAcrylate',
-           'LaurylAcrylate', 'DodecylAcrylate',
-           'LaurylMethacrylate', 'DodecylMethacrylate']
+__all__ = ['AlkylAcrylateUA', 'ButylAcrylateUA',
+           'LaurylAcrylateUA', 'DodecylAcrylateUA',
+           'LaurylMethacrylateUA', 'DodecylMethacrylateUA']
 
 import mbuild as mb
 
 from mbuildhelper.lib.moieties import AcrylateUA, MethacrylateUA
-from mbuildhelper.lib.recipes import AlkaneUA
+import mbuildhelper.lib.recipes as recipes
 
 
-class AlkylAcrylate(mb.Compound):
+class AlkylAcrylateUA(mb.Compound):
 
     def __init__(self, n=1, methyl=False, cap_front=True, cap_end=True, **kwargs):
-        super(AlkylAcrylate, self).__init__(**kwargs)
+        super(AlkylAcrylateUA, self).__init__(**kwargs)
 
         # add acrylate
         if methyl:
@@ -20,7 +20,7 @@ class AlkylAcrylate(mb.Compound):
             self.add(AcrylateUA(cap_front=cap_front, cap_end=cap_end), label="acrylate")
 
         # add alkyl tail
-        self.add(AlkaneUA(n=n, cap_front=False, cap_end=True), label="alkyl_tail")
+        self.add(recipes.AlkaneUA(n=n, cap_front=False, cap_end=True), label="alkyl_tail")
 
         # create bond between alkyl tail and acrylate
         mb.force_overlap(
@@ -29,31 +29,37 @@ class AlkylAcrylate(mb.Compound):
             to_positions=self['acrylate']['R']
         )
 
-
-class ButylAcrylate(AlkylAcrylate):
-
-    def __init__(self, cap_front=True, cap_end=True, **kwargs):
-        super(ButylAcrylate, self).__init__(n=4, methyl=False, cap_front=cap_front, cap_end=cap_end, **kwargs)
-
-
-class LaurylAcrylate(AlkylAcrylate):
-
-    def __init__(self, cap_front=True, cap_end=True, **kwargs):
-        super(LaurylAcrylate, self).__init__(n=12, methyl=False, cap_front=cap_front, cap_end=cap_end, **kwargs)
+        # hoist ports to acrylate level
+        if not cap_front:
+            self.add(self['acrylate']['up'], "up", containment=False)
+        if not cap_end:
+            self.add(self['acrylate']['down'], "down", containment=False)
 
 
-DodecylAcrylate = LaurylAcrylate
-
-
-class LaurylMethacrylate(AlkylAcrylate):
+class ButylAcrylateUA(AlkylAcrylateUA):
 
     def __init__(self, cap_front=True, cap_end=True, **kwargs):
-        super(LaurylMethacrylate, self).__init__(n=12, methyl=True, cap_front=cap_front, cap_end=cap_end, **kwargs)
+        super(ButylAcrylateUA, self).__init__(n=4, methyl=False, cap_front=cap_front, cap_end=cap_end, **kwargs)
 
 
-DodecylMethacrylate = LaurylMethacrylate
+class LaurylAcrylateUA(AlkylAcrylateUA):
+
+    def __init__(self, cap_front=True, cap_end=True, **kwargs):
+        super(LaurylAcrylateUA, self).__init__(n=12, methyl=False, cap_front=cap_front, cap_end=cap_end, **kwargs)
+
+
+DodecylAcrylateUA = LaurylAcrylateUA
+
+
+class LaurylMethacrylateUA(AlkylAcrylateUA):
+
+    def __init__(self, cap_front=True, cap_end=True, **kwargs):
+        super(LaurylMethacrylateUA, self).__init__(n=12, methyl=True, cap_front=cap_front, cap_end=cap_end, **kwargs)
+
+
+DodecylMethacrylateUA = LaurylMethacrylateUA
 
 
 if __name__ == '__main__':
-    butyl_acrylate = AlkylAcrylate(n=4, cap_front=True, cap_end=True)
+    butyl_acrylate = AlkylAcrylateUA(n=4, cap_front=True, cap_end=True)
     butyl_acrylate.save("butyl_acrylate_ua.mol2", overwrite=True)
