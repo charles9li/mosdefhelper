f1420d0036204dc3d84692bdf4298b6e3576fd5f
charles9li <charles9000li@gmail.com>
2022-11-11 13:57:38 -0800

    added apply method that allows setting of masses to 0

--- foyerhelper/forcefield.py
+++ foyerhelper/forcefield.py
@@ -1,10 +1,15 @@
+"""ForceField class that extends from foyer's ForceField class."""
 import os
+import warnings
 from pkg_resources import resource_filename
 
 import foyer
 
 
 class ForceField(foyer.Forcefield):
+    """Extension of Foyer's ForceField class that provides a number of
+    additional automatically-supported force fields.
+    """
 
     def __init__(self, forcefield_files=None, name=None, validation=True, debug=False):
         try:
@@ -21,3 +26,41 @@ class ForceField(foyer.Forcefield):
             else:
                 raise err
 
+    def apply(
+        self,
+        structure,
+        references_file=None,
+        use_residue_map=True,
+        assert_bond_params=True,
+        assert_angle_params=True,
+        assert_dihedral_params=True,
+        assert_improper_params=False,
+        verbose=False,
+        set_masses_to_zero=False,
+        *args,
+        **kwargs,
+    ):
+        """Apply the force field to a molecular structure. Same as
+        foyer.ForceField's apply method, but contains an additional keyword
+        argument that sets all the masses to zero.
+        """
+        with warnings.catch_warnings():
+            if not verbose:
+                warnings.simplefilter("ignore")
+            structure = super(ForceField, self).apply(structure,
+                                                      references_file=references_file,
+                                                      use_residue_map=use_residue_map,
+                                                      assert_bond_params=assert_bond_params,
+                                                      assert_angle_params=assert_angle_params,
+                                                      assert_dihedral_params=assert_dihedral_params,
+                                                      assert_improper_params=assert_improper_params,
+                                                      verbose=False,
+                                                      *args,
+                                                      **kwargs)
+
+        # set masses to zero if specified
+        if set_masses_to_zero:
+            for atom in structure.atoms:
+                atom.mass = 0.0
+
+        return structure
