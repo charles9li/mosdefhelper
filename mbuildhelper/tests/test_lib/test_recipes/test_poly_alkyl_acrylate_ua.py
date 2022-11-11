b987825b041543cd9bb5d4f3e63fdab4e3f953e4
charles9li <charles9000li@gmail.com>
2022-11-11 13:59:18 -0800

    fixed bug with circular imports

--- mbuildhelper/tests/test_lib/test_recipes/test_poly_alkyl_acrylate_ua.py
+++ mbuildhelper/tests/test_lib/test_recipes/test_poly_alkyl_acrylate_ua.py
@@ -1,6 +1,9 @@
 import unittest
 
+import mbuild as mb
+
 from mbuildhelper.lib.recipes import PolyAlkylAcrylateUA
+import foyerhelper
 
 
 class TestPolyAlkylAcrylateUA(unittest.TestCase):
@@ -12,6 +15,15 @@ class TestPolyAlkylAcrylateUA(unittest.TestCase):
     def test_butyl_acrylate_5mer(self):
         pba_5mer_ua = PolyAlkylAcrylateUA("5*A4")
         pba_5mer_ua.save("pba_5mer_ua.mol2", overwrite=True)
+
+    def test_butyl_acrylate_10mer(self):
+        pba_10mer_ua = PolyAlkylAcrylateUA("10*A4")
+        box = mb.Box(lengths=[4, 4, 4])
+        system = mb.fill_box(pba_10mer_ua, n_compounds=1, box=box)
+        trappeua = foyerhelper.ForceField(forcefield_files="trappe-ua-acrylates.xml")
+        system = trappeua.apply(system)
+        system.save("pba_10mer.gro", overwrite=True)
+        system.save("pba_10mer.top", overwrite=True)
 
     def test_lauryl_acrylate_4mer(self):
         pla_4mer_ua = PolyAlkylAcrylateUA("4*A12")
